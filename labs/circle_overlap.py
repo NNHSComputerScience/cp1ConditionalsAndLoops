7c9c14f93e9b2a19103dc3ab791b1b6dedeb4d51
Geoff Schmit <gcschmit@gmail.com>
2018-10-03 12:24:07 -0500

    added circle overlap extension program

---
+++ labs/circle_overlap.py
@@ -0,0 +1,19 @@
+# Complete the following code to test whether two circles, each
+#     having a user-defined radius and a fixed center point lying
+#     along the same horizontal line, are disjoint, overlapping, or
+#     mutually contained.
+
+x1 = float(input("Enter the position on the x-axis of the first circle: "))
+radius1 = float(input("Enter the radius of the first circle: "))
+
+x2 = float(input("Enter the position on the x-axis of the second circle: "))
+radius2 = float(input("Enter the radius of the second circle: "))
+
+# your work goes here
+
+
+
+
+
+# Extend your program to support any two circles regardless of their
+#     location (as opposed to lying on the same horizontal line).
